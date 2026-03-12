a42f3a096177da94966b2b97b0c360e51bb1323a
rajathjn <rajathjnx@gmail.com>
2026-03-12 16:18:54 +0530

    add utility functions for reading reasons from file

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"github.com/rajathjn/deny-by-default-as-a-service/cmd"
+	// "github.com/rajathjn/deny-by-default-as-a-service/cmd"
+	"github.com/rajathjn/deny-by-default-as-a-service/utils"
+	"log"
 )
 
+var positive_reasons string = "assets/reasons_y.json"
+var negative_reasons string = "assets/reasons_n.json"
+
 func main() {
-	cmd.Server()
+	reason_len, reasons_list := utils.ReadReasons(positive_reasons)
+	log.Printf("The loaded reasons list of length: %d and has a value: %s", reason_len, utils.Get_random_reason(reason_len, reasons_list))
 }
