104c345d2593b423f14bbceb80d6d3bf598b2e2f
rajathjn <rajathjnx@gmail.com>
2026-03-24 23:28:54 +0530

    add health check support for Docker

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/rajathjn/deny-by-default-as-a-service/cmd"
 )
 
 func main() {
+	// Work around for Health check mode for Docker healthcheck (scratch image has no curl/wget)
+	if len(os.Args) > 1 && os.Args[1] == "-health" {
+		response, err := http.Get("http://localhost:8080/health")
+		if err != nil || response.StatusCode != http.StatusOK {
+			fmt.Println("unhealthy")
+			os.Exit(1)
+		}
+		fmt.Println("healthy")
+		os.Exit(0)
+	}
+
 	address := "0.0.0.0:8080"
 	cmd.Server(address)
 }
