16114c662f7a8ebc29b4e377a3bb8d1014fc3b8f
rajathjn <rajathjnx@gmail.com>
2026-03-25 22:34:22 +0530

    update health check to use dynamic port configuration

--- main.go
+++ main.go
@@ -9,9 +9,16 @@ import (
 )
 
 func main() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	address := "0.0.0.0:" + port
+
 	// Work around for Health check mode for Docker healthcheck (scratch image has no curl/wget)
 	if len(os.Args) > 1 && os.Args[1] == "-health" {
-		response, err := http.Get("http://localhost:8080/health")
+		response, err := http.Get("http://localhost:" + port + "/health")
 		if err != nil || response.StatusCode != http.StatusOK {
 			fmt.Println("unhealthy")
 			os.Exit(1)
@@ -20,11 +27,6 @@ func main() {
 		os.Exit(0)
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	address := "0.0.0.0:" + port
 
 	cmd.Server(address)
 }
